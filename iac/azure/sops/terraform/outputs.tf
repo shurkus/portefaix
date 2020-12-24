480f18eccc727161c386eba26d859bc0aec6a7f4
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-12-24 18:10:23 +0100

    Add: AAD for Flux
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- iac/azure/sops/terraform/outputs.tf
+++ iac/azure/sops/terraform/outputs.tf
@@ -12,17 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#output "user_assigned_identity_id" {
-#  value       = azurerm_user_assigned_identity.sops.id
-#  description = "ID of the user assigned identity"
-#}
+output "identity_client_id" {
+ value       = azurerm_user_assigned_identity.sops.client_id
+ description = "Client ID of the user assigned identity"
+}
 
-#output "user_assigned_identity_name" {
-#  value       = azurerm_user_assigned_identity.sops.name
-#  description = "Name of the user assigned identity"
-#}
+output "identity_resource_id" {
+ value       = azurerm_user_assigned_identity.sops.id
+ description = "ID of the user assigned identity"
+}
 
-#output "sops_key_id" {
-#  value       = azurerm_key_vault_key.sops.id
-#  description = "Sops key ID"
-#}
+output "sops_key_id" {
+  value       = azurerm_key_vault_key.sops.id
+  description = "Sops key ID"
+}
